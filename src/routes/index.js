cbbb8acde558509a25af4fcc632414ef63303c0f
Andrés Molina <andresg.molina429@gmail.com>
2023-06-12 18:50:23 -0500

    act all

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,12 +1,15 @@
 const userRoutes = require("./users.routes");
 const authRoutes = require("./auth.routes");
 const productRoutes = require("./product.routes");
+const swaggerUi = require("swagger-ui-express");
+const swaggerDoc = require("../swagger.json");
 
 // recibe como parametro una instancia de express
 const apiRoutes = (app) => {
   app.use(userRoutes);
   app.use(authRoutes);
   app.use(productRoutes);
+  app.use("/docs", swaggerUi.serve, swaggerUi.setup(swaggerDoc));
 };
 
 module.exports = apiRoutes;
